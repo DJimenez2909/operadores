f306d3a4754f9b321b72977deebc971ba06f2db4
Duban Jimenez <duvanj390@gmail.com>
2024-09-26 16:49:27 -0500

    se crea el proyecto con lo jemplos com los operadores

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,31 @@
+#ejemplos con operadores  de asignacion
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+#var1 = var1 +3 asignacion con suma
+var1 += 3
+print(f"asignacion  con suma: {var1}")
+
+#var2 = var2 -12 asignacion con resta
+var2 -= 3
+print(f"asignacion  con resta: {var2}")
+
+#var3 = var3 -12 asignacion con multiplicacion
+var3 *= 3
+print(f"asignacion  con multiplicacion: {var3}")
+
+#var4 = var4 /14 asignacion con division
+var4 -= 3
+print(f"asignacion  con division: {var4}")
+
+#var5 = var5 6 asignacion con residuo
+var5 %= 3
+print(f"asignacion  con residuo: {var5}")
+
+#var6 = var6 8 asignacion con divsion entera
+var6 //= 12
+print(f"asignacion  con division entera: {var6}")
+
